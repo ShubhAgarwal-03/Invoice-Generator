e4678d5486b8bf717bac3126901d77c82ac75fc6
Shubh Agarwal <shubhagarwal031@gmail.com>
2026-04-27 09:20:50 +0530

    fix: remove duplicate next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -4,13 +4,3 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  }
-}
-
-module.exports = nextConfig
